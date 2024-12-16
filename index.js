d611028f9d0cb83574f22a37d2170a623dd1e316
butterdogg1467 <momink2600@gmail.com>
2024-12-15 21:04:08 -0500

    Added timer

---
+++ index.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+let date = new Date("Dec 25, 2024 00:00:00").getTime()
+
+let x = setInterval(function() {
+    let now = new Date().getTime()
+    let distance = date - now
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = days + 'd:' + hours + 'h:' + minutes + 'm:' + seconds + 's'
+
+    if (distance < 0) {
+        clearInterval(x)
+        document.getElementById("countdown").innerHTML = "EXPIRED"
+    }
+}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
